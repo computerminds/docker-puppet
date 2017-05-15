c7796993eb0d5a51b45549d67fd12943a130ec6d
Steven Jones <steven.jones@computerminds.co.uk>
2017-05-15 13:59:28 +0100

    Do the Puppet validating in parallel.

--- entry.sh
+++ entry.sh
@@ -6,13 +6,13 @@ echo $1
 
 if [ "$1" = "parser" ]
 then
-	cd puppet/ && find -name '*.pp' | xargs -n 1 -t puppet parser validate
+	cd puppet/ && find -name '*.pp' | xargs -n 1 -t -P 4 puppet parser validate
 elif [ "$1" = "linter" ]
 then
 	cd puppet/ && puppet-lint modules/*
 elif [ "$1" = "apply" ]
 then
-	puppet apply --modulepath=/root/puppet/modules/ /root/puppet/manifests/site.pp 
-else 
+	puppet apply --modulepath=/root/puppet/modules/ /root/puppet/manifests/site.pp
+else
 	echo "Don't forget to mount the volume. Use 'parser' or 'linter'"
 fi
